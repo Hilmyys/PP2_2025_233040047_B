d65b313befe935b3458ca8dc68368b30a2330e45
hilmyys <hilmiyr123@gmail.com>
2025-10-29 17:02:45 +0700

    selesai

---
+++ src/main/java/id/ac/unpas/praktikumpemogram2/PraktikumPemogram2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.ac.unpas.praktikumpemogram2;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class PraktikumPemogram2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
